5b5946d2bd56cf9558ea02e7c846c7d93c411f3b
Nai466 <58236481+Nai466@users.noreply.github.com>
2020-03-25 17:38:17 -0400

    Update Quorridor.py

--- Quorridor.py
+++ Quorridor.py
@@ -29,3 +29,31 @@ def construire_graphe(joueurs, murs_horizontaux, murs_verticaux):
         graphe.remove_edge((x, y), (x, y-1))
         graphe.remove_edge((x+1, y-1), (x+1, y))
         graphe.remove_edge((x+1, y), (x+1, y-1))
+
+    for x, y in murs_verticaux:
+        graphe.remove_edge((x-1, y), (x, y))
+        graphe.remove_edge((x, y), (x-1, y))
+        graphe.remove_edge((x-1, y+1), (x, y+1))
+        graphe.remove_edge((x, y+1), (x-1, y+1))
+
+    for joueur in map(tuple, joueurs):
+
+        for prédécesseur in list(graphe.predecessors(joueur)):
+            graphe.remove_edge(prédécesseur, joueur)
+
+            successeur = (2*joueur[0]-prédécesseur[0],
+                          2*joueur[1]-prédécesseur[1])
+
+            if successeur in graphe.successors(joueur) and successeur not in joueurs:
+                graphe.add_edge(prédécesseur, successeur)
+
+            else:
+                for successeur in list(graphe.successors(joueur)):
+                    if prédécesseur != successeur and successeur not in joueurs:
+                        graphe.add_edge(prédécesseur, successeur)
+
+    for x in range(1, 10):
+        graphe.add_edge((x, 9), 'B1')
+        graphe.add_edge((x, 1), 'B2')
+
+    return graphe
